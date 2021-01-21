5fbc99aa0d23e75da94d464ab0ed4786a3abaf00
KaterinaKourti <katherinekourti@gmail.com>
2021-01-21 20:08:13 +0200

    Modified app.py to create db tables too.

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # in order to know when an objet has changed, but not been saved to the database
 app.secret_key = 'jose'
@@ -28,4 +32,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
 
-    
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
